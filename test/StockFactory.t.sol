d5ac662fe6ff024080fa87f0090f4f4243c4b042
Joe Jordan <dev@ignitesoft.com>
2022-08-09 12:46:16 -0400

    ah, fixed it

--- test/StockFactory.t.sol
+++ test/StockFactory.t.sol
@@ -18,11 +18,16 @@ contract StockFactoryTest is PRBTest, Cheats {
     }
 
     function testCreateCompanyStock() public {
-        ICompanyStock.StockType memory stockType;
-        stockType.name = "common";
-        stockType.totalSupply = 6969 ether;
-        // i dunno, maybe this is not the best way to approach this shit
-        ICompanyStock.StockType memory myStockTypeArray = new ICompanyStock.StockType[];
+        ICompanyStock.StockType memory stockType1;
+        ICompanyStock.StockType memory stockType2;
+        stockType1.name = "common";
+        stockType1.totalSupply = 6969 ether;
+        stockType2.name = "Type A";
+        stockType2.totalSupply = 69 ether;
+
+        ICompanyStock.StockType[] memory myStockTypeArray = new ICompanyStock.StockType[](2);
+        myStockTypeArray[0] = stockType1;
+        myStockTypeArray[1] = stockType2;
 
         address msft = factory.createCompanyStock("Microsoft", "MSFT", myStockTypeArray);
         console.logAddress(msft);
