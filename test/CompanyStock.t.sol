7f7d0c18187dea74547c69d16b65a26e2fa3fe88
Joe Jordan <dev@ignitesoft.com>
2022-11-06 14:49:13 -0500

    forge-std v1 update

--- test/CompanyStock.t.sol
+++ test/CompanyStock.t.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.15;
 
 import { PRBTest } from "prb-test/PRBTest.sol";
-import { Cheats } from "forge-std/Cheats.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
 import { console } from "forge-std/console.sol";
 import { StockFactory } from "src/StockFactory.sol";
 import { ICompanyStock } from "src/interfaces/ICompanyStock.sol";
 
-contract CompanyStockTest is PRBTest, Cheats {
+contract CompanyStockTest is PRBTest, StdCheats {
     StockFactory public factory;
 
     function setUp() public {
