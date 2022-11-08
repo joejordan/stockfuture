e90014dae0fd9c069149d229eaf726174a319870
Joe Jordan <dev@ignitesoft.com>
2022-11-07 23:58:27 -0500

    switch to ERC3525

--- src/CompanyStock.sol
+++ src/CompanyStock.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
-// import { ERC1155 } from "solmate/tokens/ERC1155.sol";
-import { ERC1155 } from "@openzeppelin/token/ERC1155/ERC1155.sol";
+import { ERC3525 } from "erc-3525/ERC3525.sol";
 import { ICompanyStock } from "src/interfaces/ICompanyStock.sol";
 
-contract CompanyStock is ICompanyStock, ERC1155 {
+contract CompanyStock is ICompanyStock, ERC3525 {
     // company info
     string public companyName;
     string public tickerSymbol;
