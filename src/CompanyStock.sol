adda7f25d779f045167c08eb9f15a57fce09cc79
Joe Jordan <dev@ignitesoft.com>
2022-11-12 22:57:08 -0500

    refactor CompanyStock

--- src/CompanyStock.sol
+++ src/CompanyStock.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.15;
 
 import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import { Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import { ERC3525 } from "erc-3525/ERC3525.sol";
 import { ERC3525Burnable } from "erc-3525/ERC3525Burnable.sol";
 import { ERC3525SlotEnumerable } from "erc-3525/ERC3525SlotEnumerable.sol";
 import { ICompanyStock } from "src/interfaces/ICompanyStock.sol";
 
-contract CompanyStock is ICompanyStock, Initializable, Ownable2Step, ERC3525SlotEnumerable {
+contract CompanyStock is Initializable, ICompanyStock, Ownable2Step, ERC3525, ERC3525Burnable, ERC3525SlotEnumerable {
     // company info
     uint256 public stockTypeCount;
 
@@ -15,18 +16,24 @@ contract CompanyStock is ICompanyStock, Initializable, Ownable2Step, ERC3525Slot
 
     address public factory;
 
-    constructor(
+    function initialize(
         string memory name_,
         string memory symbol_,
         uint8 decimals_
-    ) ERC3525SlotEnumerable(name_, symbol_, decimals_) {
-        // solhint-disable-previous-line no-empty-blocks
+    ) public virtual override(ERC3525Burnable, ERC3525SlotEnumerable) initializer {
+        __ERC3525Burnable_init(name_, symbol_, decimals_);
     }
 
-    function initialize(
-        StockType[] calldata _stockTypes
-    ) public initializer returns (bool) {
-        require(msg.sender == factory, "StockFuture: `initialize` can only be called from the Factory");
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC3525, ERC3525SlotEnumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function addStockTypes(StockType[] calldata _stockTypes) public override onlyOwner returns (bool) {
         for (uint256 i = 0; i < _stockTypes.length; i++) {
             stockTypes[i] = _stockTypes[i];
         }
@@ -35,8 +42,33 @@ contract CompanyStock is ICompanyStock, Initializable, Ownable2Step, ERC3525Slot
     }
 
     function addStockType(StockType calldata _stockType) public onlyOwner returns (bool) {
-        stockTypes[stockTypeCount + 1] = _stockType;
+        stockTypeCount += 1;
+        stockTypes[stockTypeCount] = _stockType;
+
         // mint(mintTo_, tokenId_, slot_, value_);
+        // mint(_msgSender(), tokenId_, slot_, value_);
         return true;
+    }
+
+    function _beforeValueTransfer(
+        address from_,
+        address to_,
+        uint256 fromTokenId_,
+        uint256 toTokenId_,
+        uint256 slot_,
+        uint256 value_
+    ) internal override(ERC3525, ERC3525SlotEnumerable) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    function _afterValueTransfer(
+        address from_,
+        address to_,
+        uint256 fromTokenId_,
+        uint256 toTokenId_,
+        uint256 slot_,
+        uint256 value_
+    ) internal override(ERC3525, ERC3525SlotEnumerable) {
+        // solhint-disable-previous-line no-empty-blocks
     }
 }
