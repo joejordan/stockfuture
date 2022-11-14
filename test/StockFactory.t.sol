a15552282ea5caa53c64a8d4a3cf572efa87818d
Joe Jordan <dev@ignitesoft.com>
2022-11-13 23:37:03 -0500

    output some addresses

--- test/StockFactory.t.sol
+++ test/StockFactory.t.sol
@@ -7,14 +7,26 @@ import { console } from "forge-std/console.sol";
 import { PRBTest } from "@prb/test/PRBTest.sol";
 
 import { StockFactory } from "src/StockFactory.sol";
+import { CompanyStock } from "src/CompanyStock.sol";
 import { ICompanyStock } from "src/interfaces/ICompanyStock.sol";
 
 /// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
 /// https://book.getfoundry.sh/forge/writing-tests
 contract StockFactoryTest is PRBTest, StdCheats {
     StockFactory public factory;
+    CompanyStock public companyStock;
     function setUp() public {
+        vm.startPrank(address(0x69));
         factory = new StockFactory();
+        companyStock = new CompanyStock();
+        console.log("companyStock Owner:", companyStock.owner());
+        companyStock.initialize("TESTERz", "TST", 18);
+        console.log("companyStock Owner:", companyStock.owner());
+
+        console.log("Me:", msg.sender);
+        // console.log("Factory:", address(factory));
+        // console.log("companyStock:", address(companyStock));
+
     }
 
     function testCreateCompanyStock() public {
@@ -36,5 +48,17 @@ contract StockFactoryTest is PRBTest, StdCheats {
         assertEq(msft, factory.allStocks(0));
         assertEq(appl, factory.allStocks(1));
         assertEq(factory.allStocksCount(), 2);
+    }
+
+    function testCheckOwnership() public {
+        console.log("Owner:", companyStock.owner());
+    }
+
+    function testCheckFactory() public {
+        console.log("Factory:", address(factory));
+    }
+
+    function createCompany() private returns(address) {
+        return factory.createCompanyStock("Ignite Software", "IGS");
     }
 }
